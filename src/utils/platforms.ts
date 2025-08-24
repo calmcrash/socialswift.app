e90d85f4318336c5421622c463c3721778a6fa4d
calmcrash <calmcrash@gmail.com>
2025-08-24 17:15:35 -0400

    Update platforms.ts
    Updated platfdorms to include all 77 platforms1

--- src/utils/platforms.ts
+++ src/utils/platforms.ts
@@ -2,213 +2,576 @@ import { Platform } from '../types';
 
 export const platforms: Platform[] = [
   {
-    id: 'facebook',
-    name: 'Facebook',
-    icon: 'https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico',
+    id: 'alignable',
+    name: 'Alignable',
+    icon: 'alignable',
     connected: false,
     enabled: true
   },
   {
-    id: 'youtube',
-    name: 'YouTube',
-    icon: 'https://www.youtube.com/s/desktop/7449ebf7/img/favicon_144x144.png',
+    id: 'amino',
+    name: 'Amino',
+    icon: 'amino',
     connected: false,
     enabled: true
   },
   {
-    id: 'instagram',
-    name: 'Instagram',
-    icon: 'https://www.instagram.com/static/images/ico/favicon-192.png/68d99ba29cc8.png',
+    id: 'angellist',
+    name: 'AngelList',
+    icon: 'angellist',
     connected: false,
     enabled: true
   },
   {
-    id: 'tiktok',
-    name: 'TikTok',
-    icon: 'https://sf16-scmcdn-sg.ibytedtos.com/goofy/tiktok/web/node/_next/static/images/logo-whole-c555aa707602e714ec956ac96e9db366.svg',
+    id: 'artstation',
+    name: 'ArtStation',
+    icon: 'artstation',
     connected: false,
     enabled: true
   },
   {
-    id: 'pinterest',
-    name: 'Pinterest',
-    icon: 'https://s.pinimg.com/webapp/logo_trans_144x144-5e37c0c6.png',
+    id: 'baidu-tieba',
+    name: 'Baidu Tieba',
+    icon: 'baidu-tieba',
     connected: false,
     enabled: true
   },
   {
-    id: 'snapchat',
-    name: 'Snapchat',
-    icon: 'https://www.snapchat.com/static/app-ads/landing/images/snapchat-app-icon.png',
+    id: 'behance',
+    name: 'Behance',
+    icon: 'behance',
     connected: false,
     enabled: true
   },
   {
-    id: 'telegram',
-    name: 'Telegram',
-    icon: 'https://telegram.org/img/website_icon.svg',
+    id: 'bilibili',
+    name: 'Bilibili',
+    icon: 'bilibili',
     connected: false,
     enabled: true
   },
   {
-    id: 'x',
-    name: 'X',
-    icon: 'https://abs.twimg.com/responsive-web/client-web/icon-ios.b1fc727a.png',
+    id: 'bitchute',
+    name: 'BitChute',
+    icon: 'bitchute',
     connected: false,
     enabled: true
   },
   {
-    id: 'linkedin',
-    name: 'LinkedIn',
-    icon: 'https://static.licdn.com/aero-v1/sc/h/al2o9zrvru7aqj8e1x2rzsrca',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'reddit',
-    name: 'Reddit',
-    icon: 'https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'threads',
-    name: 'Threads',
-    icon: 'https://static.cdninstagram.com/rsrc.php/v3/yR/r/lam-fZmwmvn.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'discord',
-    name: 'Discord',
-    icon: 'https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6ca814282eca7172c6_icon_clyde_white_RGB.svg',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'quora',
-    name: 'Quora',
-    icon: 'https://qsf.fs.quoracdn.net/-4-ans_frontend_assets.favicon-new.ico-26-1b9e62f375da76d6.ico',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'tumblr',
-    name: 'Tumblr',
-    icon: 'https://assets.tumblr.com/images/favicons/favicon.ico',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'medium',
-    name: 'Medium',
-    icon: 'https://miro.medium.com/v2/1*m-R_BkNf1Qjr1YbyOIJY2w.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'truth',
-    name: 'Truth Social',
-    icon: 'https://truthsocial.com/packs/media/images/truth-social-logo-round-a4aa94e5.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'mastodon',
-    name: 'Mastodon',
-    icon: 'https://joinmastodon.org/favicon.svg',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'rumble',
-    name: 'Rumble',
-    icon: 'https://rumble.com/img/favicon-192x192.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'bluesky',
-    name: 'BlueSky',
-    icon: 'https://bsky.app/static/apple-touch-icon.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'substack',
-    name: 'Substack Notes',
-    icon: 'https://substackcdn.com/image/fetch/w_256,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack.com%2Fimg%2Fsubstack_icon.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'gab',
-    name: 'Gab',
-    icon: 'https://gab.com/favicon.ico',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'bereal',
-    name: 'BeReal',
-    icon: 'https://bere.al/favicon.ico',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'mix',
-    name: 'Mix.com',
-    icon: 'https://mix.com/favicon.ico',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'vk',
-    name: 'VK',
-    icon: 'https://vk.com/images/icons/pwa/apple/default.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'ok',
-    name: 'OK.ru',
-    icon: 'https://ok.ru/favicon.ico',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'google-business',
-    name: 'Google Business',
-    icon: 'https://www.gstatic.com/images/branding/product/2x/google_business_profile_192px.png',
-    connected: false,
-    enabled: true
-  },
-  {
-    id: 'vimeo',
-    name: 'Vimeo',
-    icon: 'https://i.vimeocdn.com/favicon/main-touch_180',
+    id: 'bitclout',
+    name: 'BitClout',
+    icon: 'bitclout',
     connected: false,
     enabled: true
   },
   {
     id: 'blogger',
     name: 'Blogger',
-    icon: 'https://www.blogger.com/favicon.ico',
+    icon: 'blogger',
     connected: false,
     enabled: true
   },
   {
-    id: 'wordpress',
-    name: 'WordPress',
-    icon: 'https://s1.wp.com/i/favicon.ico',
+    id: 'bluesky',
+    name: 'BlueSky',
+    icon: 'bluesky',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'caffeine',
+    name: 'Caffeine',
+    icon: 'caffeine',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'clapper',
+    name: 'Clapper',
+    icon: 'clapper',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'dailymotion',
+    name: 'Dailymotion',
+    icon: 'dailymotion',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'deso-protocol',
+    name: 'DeSo Protocol',
+    icon: 'deso-protocol',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'dev-to',
+    name: 'Dev.to',
+    icon: 'dev-to',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'deviantart',
+    name: 'DeviantArt',
+    icon: 'deviantart',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'discord',
+    name: 'Discord',
+    icon: 'discord',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'dlive',
+    name: 'DLive',
+    icon: 'dlive',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'dribbble',
+    name: 'Dribbble',
+    icon: 'dribbble',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'ello',
+    name: 'Ello',
+    icon: 'ello',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'eventbrite',
+    name: 'Eventbrite',
+    icon: 'eventbrite',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'facebook',
+    name: 'Facebook',
+    icon: 'facebook',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'farcaster',
+    name: 'Farcaster',
+    icon: 'farcaster',
     connected: false,
     enabled: true
   },
   {
     id: 'flickr',
     name: 'Flickr',
-    icon: 'https://combo.staticflickr.com/pw/images/favicons/favicon-196.png',
+    icon: 'flickr',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'gab',
+    name: 'Gab',
+    icon: 'gab',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'gettr',
+    name: 'Gettr',
+    icon: 'gettr',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'glass',
+    name: 'Glass',
+    icon: 'glass',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'google-business',
+    name: 'Google Business Profile',
+    icon: 'google-business',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'hashnode',
+    name: 'Hashnode',
+    icon: 'hashnode',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'hive-social',
+    name: 'Hive Social',
+    icon: 'hive-social',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'indie-hackers',
+    name: 'Indie Hackers',
+    icon: 'indie-hackers',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'instagram',
+    name: 'Instagram',
+    icon: 'instagram',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'kakaotalk',
+    name: 'KakaoTalk',
+    icon: 'kakaotalk',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'kick',
+    name: 'Kick',
+    icon: 'kick',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'lemon8',
+    name: 'Lemon8',
+    icon: 'lemon8',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'lens-protocol',
+    name: 'Lens Protocol',
+    icon: 'lens-protocol',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'likee',
+    name: 'Likee',
+    icon: 'likee',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'line',
+    name: 'LINE',
+    icon: 'line',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'linkedin',
+    name: 'LinkedIn',
+    icon: 'linkedin',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'mastodon',
+    name: 'Mastodon',
+    icon: 'mastodon',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'medium',
+    name: 'Medium',
+    icon: 'medium',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'meetup',
+    name: 'Meetup',
+    icon: 'meetup',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'mewe',
+    name: 'MeWe',
+    icon: 'mewe',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'minds',
+    name: 'Minds',
+    icon: 'minds',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'naver-band',
+    name: 'Naver Band',
+    icon: 'naver-band',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'newgrounds',
+    name: 'Newgrounds',
+    icon: 'newgrounds',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'nextdoor',
+    name: 'Nextdoor',
+    icon: 'nextdoor',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'ninegag-tv',
+    name: '9GAG TV',
+    icon: 'ninegag-tv',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'odnoklassniki',
+    name: 'Odnoklassniki',
+    icon: 'odnoklassniki',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'odysee',
+    name: 'Odysee',
+    icon: 'odysee',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'parler',
+    name: 'Parler',
+    icon: 'parler',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'peertube',
+    name: 'PeerTube',
+    icon: 'peertube',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'pinterest',
+    name: 'Pinterest',
+    icon: 'pinterest',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'quora',
+    name: 'Quora',
+    icon: 'quora',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'quora-spaces',
+    name: 'Quora Spaces',
+    icon: 'quora-spaces',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'reddit',
+    name: 'Reddit',
+    icon: 'reddit',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'rumble',
+    name: 'Rumble',
+    icon: 'rumble',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'snapchat',
+    name: 'Snapchat',
+    icon: 'snapchat',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'spoutible',
+    name: 'Spoutible',
+    icon: 'spoutible',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'steemit',
+    name: 'Steemit',
+    icon: 'steemit',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'substack',
+    name: 'Substack',
+    icon: 'substack',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'telegram',
+    name: 'Telegram',
+    icon: 'telegram',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'threads',
+    name: 'Threads',
+    icon: 'threads',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'tiktok',
+    name: 'TikTok',
+    icon: 'tiktok',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'triller',
+    name: 'Triller',
+    icon: 'triller',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'truth-social',
+    name: 'Truth Social',
+    icon: 'truth-social',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'tumblr',
+    name: 'Tumblr',
+    icon: 'tumblr',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'twitch',
+    name: 'Twitch',
+    icon: 'twitch',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'vero',
+    name: 'Vero',
+    icon: 'vero',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'vimeo',
+    name: 'Vimeo',
+    icon: 'vimeo',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'vk',
+    name: 'VK',
+    icon: 'vk',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'vsco',
+    name: 'VSCO',
+    icon: 'vsco',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'weibo',
+    name: 'Weibo',
+    icon: 'weibo',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'wordpress',
+    name: 'WordPress.com',
+    icon: 'wordpress',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'x-twitter',
+    name: 'X/Twitter',
+    icon: 'x-twitter',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'xiaohongshu',
+    name: 'Xiaohongshu',
+    icon: 'xiaohongshu',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'youtube',
+    name: 'YouTube',
+    icon: 'youtube',
+    connected: false,
+    enabled: true
+  },
+  {
+    id: 'zynn',
+    name: 'Zynn',
+    icon: 'zynn',
     connected: false,
     enabled: true
   }
 ];
+
+// Utility function to get platform icon path with fallback formats
+export const getPlatformIconPath = (iconName: string): string => {
+  // Try different formats in order of preference: SVG, PNG, JPG, JPEG
+  const formats = ['svg', 'png', 'jpg', 'jpeg'];
+  
+  // For build time, you'll need to import or check which format exists
+  // For now, we'll default to PNG as you mentioned that's what you're finding most
+  return `/src/assets/icons/${iconName}.png`;
+};
+
+// Alternative: if you want to handle the format checking at runtime
+export const getPlatformIconWithFallback = (iconName: string): string => {
+  // You can implement logic here to check which format exists
+  // or use a try/catch with image loading to fallback
+  const baseUrl = `/src/assets/icons/${iconName}`;
+  
+  // Default to PNG since you mentioned that's most common
+  return `${baseUrl}.png`;
+};
